9855221b84824ec863eb2c485c1935e025cde08f
Andr <diushes.klassniy@gmail.com>
2025-04-21 20:09:45 +0300

    Add Header structure

---
+++ src/Layout/header/Header.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo>Logo</Logo>
+            <Menu>
+                <MenuItem>
+                    <MenuLink>
+                        Home
+                    </MenuLink>
+                </MenuItem>
+                <MenuItem>
+                    <MenuLink>
+                        About
+                    </MenuLink>
+                </MenuItem>
+                <MenuItem>
+                    <MenuLink>
+                        Services
+                    </MenuLink>
+                </MenuItem>
+            </Menu>
+            <ContactButton></ContactButton>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+    
+`
+
+const Logo = styled.span`
+    
+`
+
+const Menu = styled.ul`
+    
+`
+
+const MenuItem = styled.li`
+    
+`
+
+const MenuLink = styled.a`
+
+`
+
+const ContactButton = styled.button`
+    
+`
